b299d41c700155c56944027a41053c1770cd8d45
Rishabh Tiwari <onlinestuff.rishabh@gmail.com>
2022-02-10 22:40:34 +0530

    Update day_28.java

--- Thirty_days_of_Code_2022/day_28.java
+++ Thirty_days_of_Code_2022/day_28.java
@@ -15,91 +15,32 @@ public class day_28 {
         }
         return min_idx;
     }
+class TestDataEmptyArray {
+    public static int[] get_array() {
+        int[] a = { 8, 7, 9, 4, 2, 1, 6, 8, 5 };
+        return a;
+    }
+}
 
-static class TestDataEmptyArray {
-        public static int[] get_array() {
-        int arr[]={};
-        return arr;
-        }
-    }
-    static class TestDataUniqueValues {
-        public static int[] get_array() {
-            // complete this function
-            int arr[]={1,2,3,4,5};
-        return arr;
-        }
-        public static int get_expected_result() {
-            // complete this function
-            return 0;
-        }
-    }
-    static class TestDataExactlyTwoDifferentMinimums {
-        public static int[] get_array() {
-            // complete this function
-            int arr[]={1,3,1};
-        return arr;
-        }
-        public static int get_expected_result() {
-            // complete this function
-            return 0;
-        }
+ class TestDataExactlyTwoDifferentMinimums {
+    public static int[] get_array() {
+        int[] a = { 8, 7, 9, 7, 2, 1, 6, 8, 5 };
+        return a;
     }
 
-    
-	public static void TestWithEmptyArray() {
-        try {
-            int[] seq = TestDataEmptyArray.get_array();
-            int result = minimum_index(seq);
-        } catch (IllegalArgumentException e) {
-            return;
-        }
-        throw new AssertionError("Exception wasn't thrown as expected");
+    public static int get_expected_result() {
+        return 1;
+    }
+}
+
+ class TestDataUniqueValues {
+    public static int[] get_array() {
+        int[] a = { 8, 7, 9, 4, 2, 1, 6, 8, 5 };
+        return a;
     }
 
-    public static void TestWithUniqueValues() {
-        int[] seq = TestDataUniqueValues.get_array();
-        if (seq.length < 2) {
-            throw new AssertionError("less than 2 elements in the array");
-        }
-
-        Integer[] tmp = new Integer[seq.length];
-        for (int i = 0; i < seq.length; ++i) {
-            tmp[i] = Integer.valueOf(seq[i]);
-        }
-        if (!((new LinkedHashSet<Integer>(Arrays.asList(tmp))).size() == seq.length)) {
-            throw new AssertionError("not all values are unique");
-        }
-
-        int expected_result = TestDataUniqueValues.get_expected_result();
-        int result = minimum_index(seq);
-        if (result != expected_result) {
-            throw new AssertionError("result is different than the expected result");
-        }
+    public static int get_expected_result() {
+        return 5;
     }
-
-    public static void TestWithExactlyTwoDifferentMinimums() {
-        int[] seq = TestDataExactlyTwoDifferentMinimums.get_array();
-        if (seq.length < 2) {
-            throw new AssertionError("less than 2 elements in the array");
-        }
-
-        int[] tmp = seq.clone();
-        Arrays.sort(tmp);
-        if (!(tmp[0] == tmp[1] && (tmp.length == 2 || tmp[1] < tmp[2]))) {
-            throw new AssertionError("there are not exactly two minimums in the array");
-        }
-
-        int expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result();
-        int result = minimum_index(seq);
-        if (result != expected_result) {
-            throw new AssertionError("result is different than the expected result");
-        }
-    }
-
-    public static void main(String[] args) {
-        TestWithEmptyArray();
-        TestWithUniqueValues();
-        TestWithExactlyTwoDifferentMinimums();
-        System.out.println("OK");
-    }
+}    
 }
