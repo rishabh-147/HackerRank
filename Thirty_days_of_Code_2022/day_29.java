4feb702b0b4fe5ad4b000ae1b6516f5279ee76e0
Rishabh Tiwari <onlinestuff.rishabh@gmail.com>
2022-02-10 22:40:23 +0530

    Update day_29.java

--- Thirty_days_of_Code_2022/day_29.java
+++ Thirty_days_of_Code_2022/day_29.java
@@ -1,40 +1,41 @@
-package Thirty_days_of_Code_2022;
 
+package Thirty_days_of_Code_2022;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class day_29 {
+public class day_29
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		
-		Scanner ob = new Scanner(System.in);
-		int n = ob.nextInt();
-		String[][] arr = new String[n][2];
+        
+        Scanner ob = new Scanner(System.in);
+        int n = ob.nextInt();
+        String[][] arr = new String[n][2];
 
-			for(int i = 0;  i < n; i++) {
-				arr[i][0]= ob.next();
-				arr[i][1]= ob.next();
-				//System.out.println(i);
-						
-		}
-		
-		ArrayList<String> set  = new ArrayList<>(6);
-		for(int i = 0 ; i < n; i++) {
-			//System.out.println("########");
-		
-		 if(arr[i][1].substring(arr[i][1].indexOf('@')).equalsIgnoreCase("@gmail.com")) 
-			 set.add(arr[i][0]);
-		 
-		}
-		Collections.sort(set);
-		for(String i : set) {
-			System.out.println(i);
-		}
-	}
-	
-	
+            for(int i = 0;  i < n; i++) {
+                arr[i][0]= ob.next();
+                arr[i][1]= ob.next();
+                //System.out.println(i);
+                        
+        }
+        
+        ArrayList<String> set  = new ArrayList<>(6);
+        for(int i = 0 ; i < n; i++) {
+            //System.out.println("########");
+        
+         if(arr[i][1].substring(arr[i][1].indexOf('@')).equalsIgnoreCase("@gmail.com")) 
+             set.add(arr[i][0]);
+         
+        }
+        Collections.sort(set);
+        for(String i : set) {
+            System.out.println(i);
+        }
+    }
+    
+    
 
 }
