5397b3a467f4727753bf316d0979f8a1efab68c2
Rishabh Tiwari <onlinestuff.rishabh@gmail.com>
2022-02-11 09:27:11 +0530

    Update day_30.java

--- Thirty_days_of_Code_2022/day_30.java
+++ Thirty_days_of_Code_2022/day_30.java
@@ -3,7 +3,38 @@ package Thirty_days_of_Code_2022;
 import java.util.*;
 
 public class day_30 {
+	
+	// APPROACH 1 --> Less Time Complexity
+		    public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+	        Scanner ob = new Scanner(System.in);
+	        int T = ob.nextInt();
 
+	        while (T-- > 0) {
+	            int N = ob.nextInt();
+	            int K = ob.nextInt();
+	            int max = 0;
+	            
+	            for(int i = 1; i < N; i++) {
+	                int and = 0;
+	                for(int j = i +1; j <= N; j++) {
+	                   and = (i & j);
+	                    if((and) < K && and > max)
+	                        max = and;
+	                }
+	            }
+	            System.out.println(max);
+	            
+	        }
+	    ob.close();
+		    }
+	    
+	}
+
+
+
+//APPROACH 2 --> Time Complexity High
+/*
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner ob = new Scanner(System.in);
@@ -12,22 +43,25 @@ public class day_30 {
 		while (T-- > 0) {
 			int N = ob.nextInt();
 			int K = ob.nextInt();
-			int and = 0;
-			ArrayList<Integer> arr = new ArrayList<>(N);
+			int max = 0;
 			
-			for(int i = 1; i <= N; i++)
-			{
-				arr.(i, new ArrayList<Integer>());
-			}
 			for(int i = 1; i < N; i++) {
+				ArrayList<Integer> arr = new ArrayList<>(N);
+				
 				for(int j = i +1; j <= N; j++) {
-					and = (i & j);
+					if((i & j) < K)
+					arr.add((i & j));
 					
 				}
+				Collections.sort(arr);
+				if(arr.size() > 0 && arr.get(arr.size() - 1) > max) max  = arr.get(arr.size() - 1);
 			}
-			System.out.println(and);
+			System.out.println(max);
 			
 		}
 	}
 	
 }
+
+*/
+
